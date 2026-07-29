9902c8eccd0cfa06ce5a1d55dc5448f585f4d023
priyommukherjee53-netizen <priyommukherjee53@gmail.com>
2026-07-29 21:32:18 +0530

    Merge Redux Experiments 1.2.1 and 1.2.2 into one project

--- EXP 2/EXP 1.2.1/src/features/posts/postsSelectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit";
-
-const selectPosts = (state) => state.posts.posts;
-
-export const selectLinkedInPosts = createSelector(
-  [selectPosts],
-  (posts) =>
-    posts.filter((post) => post.platform === "LinkedIn")
-);
